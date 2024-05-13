2e51e0c6b280b5325abce87af95b178244e22c58
Babaloga <31783107+Babaloga@users.noreply.github.com>
2024-05-13 14:57:26 -0400

    first commit

---
+++ js/Header.js
@@ -0,0 +1,32 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+      <img src="heart.png" alt="An anatomical heart made of computer cables">
+      <div id="nav-header">
+          <h2>George Eltzroth</h2>
+          <h3>Babaloga</h3>
+      </div>
+      <menu>
+          <a href="home.html">
+              <li id="desktop-li">◧ Home ◨</li>
+              <li id="mobile-li">◧ Home</li>
+          </a>
+          <a href="games.html">
+              <li id="desktop-li">◭ Portfolio ◮</li>
+              <li id="mobile-li">◭ Portfolio</li>
+          </a>
+          <a href="blog.html">
+              <li id="desktop-li">◐ Blog ◑</li>
+              <li id="mobile-li">◐ Blog</li>
+          </a>
+      </menu>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
+  
