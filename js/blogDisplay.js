dee8938d036fa633007d2de6f862347dd75488cf
Babaloga <31783107+Babaloga@users.noreply.github.com>
2024-07-01 13:57:25 -0400

    welp, fuck.

--- js/blogDisplay.js
+++ js/blogDisplay.js
@@ -55,7 +55,7 @@ class BlogFeed extends HTMLElement {
         return `
             <div class="blogpost" id="${blogDate}">
                 <h2>${blogTitle}</h2>
-                <h4>${blogdate}</h4>
+                <h4>${blogDate}</h4>
                 ${blogContent}    
             </div>
         `;
